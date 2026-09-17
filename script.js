4c5bfec9add286ead8770501bbf90eb20a8178d3
Guled Jama <guledjama@192.168.1.116>
2026-09-17 07:17:40 +0100

    generate season button

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("script is running");
+const seasonsBox = document.getElementById("seasons");
+
+for (let s = 1; s <= 9; s++) {
+  const button = document.createElement("button");
+  button.textContent = s;
+  button.dataset.season = s;
+  button.addEventListener("click", () => {
+    console.log("clicked season", s);
+  });
+  seasonsBox.appendChild(button);
+}
